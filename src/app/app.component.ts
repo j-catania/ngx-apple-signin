661357327a51d4224df37dd2899fb028bd933af4
Julien Catania <julien.catania@gmail.com>
2019-06-05 10:03:33 -0700

    comp init ok

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+declare const AppleID : any;
 
 @Component({
-  selector: 'app-root',
+  selector: 'ng8-apple-siginin',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'ng8-apple-signin';
+
+  ngOnInit(): void {
+    AppleID.auth.init({
+      clientId : '[CLIENT_ID]',
+      scope : '[SCOPES]',
+      redirectURI: '[REDIRECT_URI]',
+      state : '[STATE]'
+    });
+
+    const buttonElement = document.getElementById('sign-in-with-apple-button');
+    buttonElement.addEventListener('click', () => {
+      AppleID.auth.signIn();
+    });
+  }
 }
